7c7f79d4bc6cc4d7f9cdedd8c927fe43b0bc11a4
Sergej Sintschilin <seregpie@gmail.com>
2021-12-03 15:15:29 +0100

    update type declarations

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,25 @@
-export interface MaxDiff<T> {
-	items: T[]
-	progress: number
-	complete: boolean
-	result: T[] | null
-	getCandidates(limit?: number): T[]
-	getOrderedPairs(): [T, T][]
-	getUnorderedPairs(): [T, T][]
-	getOrderedGroups(): T[][]
-	getUnorderedGroups(): T[][]
-	getItemsBefore(item: T, otherItems: Iterable<T>): T[]
-	getItemsAfter(item: T, otherItems: Iterable<T>): T[]
-	order(...items: T[]): void
-	orderBefore(item: T): void
-	orderAfter(item: T): void
-	orderFirst(item: T): void
-	orderLast(item: T): void
-	clone(): MaxDiff<T>
+export interface MaxDiffInstance<T> {
+	items: Array<T>;
+	progress: number;
+	complete: boolean;
+	result?: Array<T>;
+	getCandidates(limit?: number): Array<T>;
+	getOrderedPairs(): Array<[T, T]>;
+	getUnorderedPairs(): Array<[T, T]>;
+	getOrderedGroups(): Array<Array<T>>;
+	getUnorderedGroups(): Array<Array<T>>;
+	getItemsBefore(item: T, otherItems: Iterable<T>): Array<T>;
+	getItemsAfter(item: T, otherItems: Iterable<T>): Array<T>;
+	order(...items: Array<T>): void;
+	orderBefore(item: T): void;
+	orderAfter(item: T): void;
+	orderFirst(item: T): void;
+	orderLast(item: T): void;
+	clone(): MaxDiffInstance<T>;
 }
 
-export default function<T>(items: Iterable<T>): MaxDiff<T>
+export interface MaxDiff<T> {
+	(items: Iterable<T>): MaxDiffInstance<T>;
+}
+
+export default MaxDiff;
